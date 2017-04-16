8ed654d7bda7d076ed496cf9409914e519dace75
Amanda Beiner <aebeiner@gmail.com>
2017-04-15 20:56:31 -0400

    turn sign in page into React

--- react/src/main.js
+++ react/src/main.js
@@ -2,6 +2,10 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>Dope</h1>, document.getElementById('app')
-);
+import SignInTile from './static/SignInTile'
+
+if(document.getElementById('app')){
+  ReactDOM.render(
+    <SignInTile/>, document.getElementById('app')
+  );
+}
