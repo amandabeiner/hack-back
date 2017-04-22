f93b5bec7a7ffe5f1e660ae8c28645a92bae8ce0
Mike Thomson <michaelgt04@gmail.com>
2017-04-22 12:48:05 -0400

    Added Google, LinkedIn and Facebook login for organizations

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-  end
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
+end
