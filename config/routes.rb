15284b2af2eeb48525a05a5285b4b9de33f31078
Scott Ehrmann <s.s.ehrmann@gmail.com>
2017-04-22 12:03:56 -0400

    api controller for users: show

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback', to: 'sessions#create'
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:show]
+    end
+  end
+
   delete '/logout', to: 'sessions#destroy'
 end
