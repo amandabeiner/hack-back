539df6c03de19d2d5d0ad308c2eae665a9762781
Mike Thomson <michaelgt04@gmail.com>
2017-04-22 13:22:01 -0400

    Setup API endpoint for organization profile

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
 
   delete '/logout', to: 'sessions#destroy'
+
+  namespace :api do
+    namespace :v1 do
+      resources :organizations, only: [:show]
+    end
+  end
 end
