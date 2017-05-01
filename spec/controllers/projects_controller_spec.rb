6c528141b14a00636adf297c4ca5ccd3a603a710
Amanda Beiner <aebeiner@gmail.com>
2017-04-30 20:07:04 -0400

    projects controller testing

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,40 +1,35 @@
 require "rails_helper"
 
 describe Api::V1::ProjectsController do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:organization) { FactoryGirl.create(:organization, user: user) }
+  let!(:project) { FactoryGirl.create(:project, organization: organization) }
+
   describe "#index" do
-    let(:user) { FactoryGirl.create(:user) }
-    let(:organization) { FactoryGirl.create(:organization, user: user) }
-    let!(:project) { FactoryGirl.create(:project, organization: organization) }
 
     let!(:expected_json) {
       {"projects"=>
         [
           {
-            "id"=>"#{project.id}",
+            "id"=>project.id,
             "name"=>"Demolish the Patriarchy",
             "description"=>"self explanatory",
             "stack"=>"Beyonce",
             "claimed"=>false,
-            "created_at"=>"#{project.created_at.iso8601}",
-            "updated_at"=>"#{project.updated_at.iso8601}",
+            "created_at"=>"#{project.created_at.iso8601(3)}",
+            "updated_at"=>"#{project.updated_at.iso8601(3)}",
             "organization"=> {
-               "id"=>"#{organization.id}",
+               "id"=>organization.id,
                 "name"=>"The best organization",
                 "description"=>"the best one. this one. obviously.",
                 "mission_statement"=>"crush it",
                 "website"=>"www.thebestorg.org",
               },
             "contact"=>{
-              "id"=>"#{user.id}",
+              "id"=>user.id,
               "name"=>"amanda beiner",
               "email"=>"myemail@me.com",
               "image_url"=>"mypicture.com",
-              "github_url"=>"github.com/amandabeiner",
-              "bio"=>nil,
-              "current_position"=>nil,
-              "years_of_experience"=>nil,
-              "skills"=>nil,
-              "interests"=>nil
             }
           }
         ]
@@ -44,6 +39,7 @@ describe Api::V1::ProjectsController do
 
     it "should return a json representation of the projects in the database" do
       get :index
+      
       parsed = JSON.parse(response.body)
 
       expect(response.content_type).to eq("application/json")
@@ -51,4 +47,41 @@ describe Api::V1::ProjectsController do
     end
   end
 
+  describe "#show" do
+    let!(:expected_json){
+      {
+        "project"=> {
+          "id"=> project.id,
+          "name"=> "Demolish the Patriarchy",
+          "description"=> "self explanatory",
+          "stack"=> "Beyonce",
+          "claimed"=> false,
+          "created_at"=> "#{project.created_at.iso8601(3)}",
+          "updated_at"=> "#{project.updated_at.iso8601(3)}",
+          "organization"=> {
+            "id"=> organization.id,
+            "name"=> "The best organization",
+            "description"=> "the best one. this one. obviously.",
+            "mission_statement"=> "crush it",
+            "website"=> "www.thebestorg.org"
+          },
+          "contact"=> {
+            "id"=> user.id,
+            "name"=> "amanda beiner",
+            "email"=> "myemail@me.com",
+            "image_url"=> "mypicture.com"
+          }
+        }
+      }
+    }
+
+    it "should return a json representation of the information for the specified project" do
+      get :show, id: project.id
+
+      parsed = JSON.parse(response.body)
+
+      expect(response.content_type).to eq("application/json")
+      expect(parsed).to eq(expected_json)
+    end
+  end
 end
