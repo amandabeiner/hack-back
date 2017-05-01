87d5e40511aaa9a8edc536f34ff9eaf7d7fbb57c
michaelgt04 <michaelgt04@gmail.com>
2017-04-30 22:15:43 -0400

    Wrote tests for ProjectsIndex and ProjectShowContainers using new Redux setup

--- react/test/projectFeed/containers/ProjectShowContainerSpec.js
+++ react/test/projectFeed/containers/ProjectShowContainerSpec.js
@@ -1,5 +1,6 @@
-import ProjectShowContainer from 'projectFeed/containers/ProjectShowContainer';
-import Project from 'projectFeed/components/Project'
+import Project from 'projectFeed/components/Project';
+import ContactPerson from 'sharedResources/components/ContactPerson';
+import OrganizationProfile from 'sharedResources/components/OrganizationProfile';
 
 describe('ProjectShowContainer', () => {
 
@@ -13,13 +14,47 @@ describe('ProjectShowContainer', () => {
     wrapper = mountReactAppAt('/projects/1')
   });
 
-  it('should render a Project component', done => {
+  it('should render a Project component with the correct props', done => {
     setTimeout(() => {
-      expect(wrapper.text()).toMatch('test project');
+      expect(wrapper.find(Project)).toBePresent()
+      expect(wrapper.find(Project).props()).toEqual(
+        {
+          name: 'test project',
+          description: 'test ability to make a project',
+          stack: 'json',
+          claimed: false
+        }
+      )
       done();
     }, 0);
   });
 
+  it('should render a ContactPerson component with the correct props', done => {
+    setTimeout(() => {
+      expect(wrapper.find(ContactPerson)).toBePresent()
+      expect(wrapper.find(ContactPerson).props()).toEqual(
+        {
+          name: 'Amanda Beiner',
+          email: 'aebeiner@gmail.com',
+          imageUrl: 'https://lh6.googleusercontent.com/-Uch-KItz22s/AAAAAAAAAAI/AAAAAAAAAbM/FDaKNpvv504/photo.jpg'
+        }
+      )
+      done()
+    }, 0)
+  });
 
-
+  it('should render an OrganizationProfile component', done => {
+    setTimeout(() => {
+      expect(wrapper.find(OrganizationProfile)).toBePresent()
+      expect(wrapper.find(OrganizationProfile).props()).toEqual(
+        {
+          name: 'Test Org',
+          description: 'testing json all day',
+          missionStatement: 'to test things thoroughly',
+          website: 'www.jsontest.org'
+        }
+      )
+      done()
+    }, 0)
+  });
 })
