618b81780cb8cc1c3c7a857ab763a78ea9027b48
Amanda Beiner <aebeiner@gmail.com>
2017-05-02 11:50:22 -0400

    model testing for Contact class methods

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,15 +2,55 @@ require "rails_helper"
 
 RSpec.describe Contact, type: :model do
 
-  describe "Contact" do
-    let!(:contact) {FactoryGirl.create(:contact)}
+  mock_auth = {
+    "provider"=>"facebook",
+    "uid"=>"123456",
+    "info"=> {
+      "name"=>"Amanda Beiner",
+      "email"=>"myemail@me.com",
+      "image"=>"mypic.com",
+    }
+  }
 
-    it "has the required attributes" do
-      expect(contact.name).to_not be_nil
-      expect(contact.email).to_not be_nil
-      expect(contact.uid)
-      expect(contact.image_url).to_not be_nil
-      expect(contact.provider).to_not be_nil
+  describe "Contact" do
+
+    it { should have_valid(:name).when("Amanda Beiner", "Mike Thomson") }
+    it { should_not have_valid(:name).when(nil) }
+
+    it { should have_valid(:email).when("marjie@killingit.com") }
+    it { should_not have_valid(:email).when(nil) }
+
+    it { should have_valid(:image_url).when("mypicture.com") }
+    it { should_not have_valid(:image_url).when(nil) }
+
+    it { should have_valid(:uid).when("123456") }
+    it { should_not have_valid(:uid).when(nil) }
+
+    it { should have_valid(:provider).when("facebook", "linkedin", "google_oauth2") }
+    it { should_not have_valid(:provider).when(nil) }
+
+    it { should have_many(:organizations) }
+  end
+
+  describe "#find_or_create_contact" do
+
+    it "changes the number of contacts in the database by 1" do
+      expect { Contact.find_or_create_contact(mock_auth) }.to change{ Contact.count }.by(1)
+    end
+
+    it "creates a contact object with the correct attributes" do
+      new_contact = Contact.find_or_create_contact(mock_auth)
+      expect(new_contact.provider).to eq("facebook")
+      expect(new_contact.name).to eq("Amanda Beiner")
+      expect(new_contact.email).to eq("myemail@me.com")
+      expect(new_contact.image_url).to eq("mypic.com")
+    end
+  end
+
+  describe "#organization_auth?" do
+    it "should return true if a contact's provider is facebook" do
+
+      expect(Contact.organization_auth?(mock_auth)).to eq(true)
     end
   end
 end
