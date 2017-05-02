1cf8afd7893515101c149bcc18d435049172473c
Mike Thomson <michaelgt04@gmail.com>
2017-05-02 17:20:32 -0400

    Fixed deprecation warnings in rspec controller tests

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -16,7 +16,7 @@ describe Api::V1::ContactsController do
     }
 
     it "should return a json representation of the specified organization" do
-      get :show, id: contact.id
+      get :show, params: { id: contact.id }
 
       parsed = JSON.parse(response.body)
       expect(response.content_type).to eq("application/json")
