2258c9903f81ce6b17e4efdfc4b5b449c951e472
Amanda Beiner <aebeiner@gmail.com>
2017-05-05 17:09:46 -0400

    finally got the div architecture down, with preliminary styling but horrible colors

--- react/src/developerProfile/components/DeveloperProfile.js
+++ react/src/developerProfile/components/DeveloperProfile.js
@@ -22,38 +22,41 @@ const DeveloperProfile = props => {
         key={skill}
         skill={skill}
       />
-    )
+    );
   })
 
   return(
-    <div className="dev-info-container">
-      <div className="basic-info-background">
+
+    <div className="dev-profile-container">
+      <div className="orange-background">
         <div className="basic-info">
-          <div className="content">
-            <img className="dev-photo" src={props.developer.image_url}/>
-            <h1 className="dev-name">{props.developer.name}</h1>
-            <DeveloperInfo developer={props.developer} />
-          </div>
+          <img className="dev-photo" src={props.developer.image_url}/>
+          <h1 className="dev-name">{props.developer.name}</h1>
+          <DeveloperInfo developer={props.developer} />
         </div>
       </div>
-      <div className="row">
-        <div className="column skills-bio-section small-4">
-          <div className="bio column small-12">
-            <h3 className="subtitle">Bio</h3>
-            <DeveloperBio
-            bio={props.developer.bio}
-            />
+
+      <div className="gray-background">
+        <div className="more-info row">
+          <div className="bio-skills column small-4">
+            <div className="bio column small-12">
+              <h3 className="subtitle">Bio</h3>
+              <DeveloperBio
+                bio={props.developer.bio}
+              />
+            </div>
+            <div className="skills column small-12">
+              <h3 className="subtitle">Skills</h3>
+              <ul>
+                {developerSkills}
+              </ul>
+            </div>
           </div>
-          <div className="skills column small-12">
-            <h3 className="subtitle">Skills</h3>
-            <ul>
-              {developerSkills}
-            </ul>
+
+          <div className="portfolio-projects column small-7">
+            <h2>Portfolio</h2>
+            {portfolioProjects}
           </div>
-        </div>
-        <div className="portfolio-projects column small-6">
-          <h2>Portfolio</h2>
-          {portfolioProjects}
         </div>
       </div>
     </div>
