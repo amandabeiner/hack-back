3b279a6c59cc1422637804f382c999523b487a97
Amanda Beiner <aebeiner@gmail.com>
2017-05-07 18:26:35 -0400

    add recent projects component to developer profile

---
+++ react/src/developerProfile/components/DeveloperProject.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const DeveloperProject = props => {
+  let completeText;
+  if(props.project.completed) {
+    completeText = "Complete"
+  } else {
+    completeText = "In Progress"
+  }
+  
+  return(
+    <div className="project-tile content-box">
+      <h3 className="subtitle">{props.project.name}</h3>
+      <ul>
+        <li className="project-description">{props.project.description}</li>
+        <li><i className="fa fa-stack-overflow" aria-hidden="true"></i> {props.project.stack}</li>
+        <li><i className="fa fa-globe" aria-hidden="true"></i> <Link to={`/organizations/${props.project.organization_id}/profile`}>Organization Profile</Link></li>
+        <li>{completeText}</li>
+      </ul>
+    </div>
+  )
+}
+
+export default DeveloperProject;
